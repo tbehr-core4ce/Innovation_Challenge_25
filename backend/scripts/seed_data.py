d00683c41e09ec350f32255f45fda00a5c000c32
Tiffany Behr <kwaiipie@gmail.com>
2025-11-12 19:30:16 -0500

    tasking

---
+++ backend/scripts/seed_data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+1. Import parser/ ingestor to then physically look for dataset files and do its thing
+
+2. Use validator sparingly just for practice
+
+3. Using python to insert data into what schema 
+
+"""
+
